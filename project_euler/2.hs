c805ed22483e6374fb26a34c052199f70c00c752
Alex Lee (aviidlee) <alex.lee@posteo.net>
2023-08-22 22:50:22 -0400

    Naive solution that doesn't work but at least we learned syntax

---
+++ project_euler/2.hs
@@ -0,0 +1,3 @@
+-- naive recursive solution is too slow, need dynamic programming.
+let fib 1 = 1; fib 2 = 2; fib n = fib (n-1) + fib (n-2)
+smallEvenFibs = [fib(n) | n <- [1,2..], n `mod` 2 == 0, fib(n) < 4000000]
