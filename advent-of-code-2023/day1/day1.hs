18fe464c7bfd84cf7ab173286c25bd2708ca763e
Alex H. S. Lee <alex.lee@posteo.net>
2023-12-10 19:03:21 -0500

    Return recovered calibration codes

--- advent-of-code-2023/day1/day1.hs
+++ advent-of-code-2023/day1/day1.hs
@@ -1,17 +1,20 @@
 import Data.Char 
-import Text.Read (readMaybe)
+import System.IO
 
 main = do
-    putStrLn "hello, what's your name?"
-    name <- getLine 
-    putStrLn ("Hey " ++ name ++ "!")
-
+    handle <- openFile "input.txt" ReadMode
+    contents <- hGetContents handle 
+    let values = lines contents
+        recovered_values = map recover_calibration_value values 
+        answer = sum recovered_values
+    print answer
 
 recover_calibration_value :: String -> Int
 recover_calibration_value input =
     read string_digits :: Int
     where digits = get_digits input
           string_digits = (head digits):[(last digits)] 
+
 
 get_digits :: String -> String
 get_digits input = filter isDigit input
