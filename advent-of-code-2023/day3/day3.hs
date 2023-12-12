6208e29135a663556f35a53c3de7bfb6faa6e4af
Alex H. S. Lee <alex.lee@posteo.net>
2023-12-11 20:42:28 -0500

    SKeleton for problem 3

---
+++ advent-of-code-2023/day3/day3.hs
@@ -0,0 +1,14 @@
+
+-- Total number of cubes of color red, green, blue
+totalCubes = (12, 13, 14)
+
+main = do
+    args <- getArgs 
+    handle <- openFile (args !! 0) ReadMode
+isSetPossible :: (Int a) => (a, a, a) -> Bool
+isSetPossible _ = error "implement me"
+
+
+isGamePossible :: (Int a) => [(a, a, a)]
+isGamePossible _ = error "implement me"
+
