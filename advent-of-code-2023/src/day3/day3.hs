e84e64f2b3dcf4b6094a3f94a56f7acab3a6fff8
Alex H. S. Lee <alex.lee@posteo.net>
2024-01-09 18:53:13 -0500

    exercism exercise 5

--- advent-of-code-2023/src/day3/day3.hs
+++ advent-of-code-2023/src/day3/day3.hs
@@ -2,7 +2,11 @@
 -- Total number of cubes of color red, green, blue
 totalCubes = (12, 13, 14)
 
-
+-- Expected format: 1 blue, 2 red, 5 green 
+getCubeSet :: String -> (Int, Int, Int)
+getCubeSet input =
+    (red, green, blue) 
+    where 
 isSetPossible :: (Int, Int, Int) -> Bool
 isSetPossible (red, green, blue) = 
     red <= first totalCubes 
