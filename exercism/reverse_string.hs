104a2a40bfdc2f39ecd0ae746a7f26947cb5898e
Alex H. S. Lee <alex.lee@posteo.net>
2024-01-21 18:29:39 -0500

    reverse string other solutions

--- exercism/reverse_string.hs
+++ exercism/reverse_string.hs
@@ -3,7 +3,10 @@
 -- First naive solution: recursion 
 reverseString :: String -> String
 reverseString [] = []
-reverseString [x] = [x]
 reverseString xs = last xs : reverseString (init xs)
 
 -- There is in fact, a library function called reverse! 
+
+-- Another way to do the recursion
+reverseString' [] = []
+reverseString' (x:xs) = reverseString xs ++ [x]
