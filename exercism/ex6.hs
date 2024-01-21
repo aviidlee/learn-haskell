efe35eb7ce28eba631b6f3957f34c250fc87d8fc
Alex H. S. Lee <alex.lee@posteo.net>
2024-01-21 18:06:15 -0500

    exercism exercise 6

--- exercism/ex6.hs
+++ exercism/ex6.hs
@@ -11,3 +11,11 @@ collatz' 1 steps = (1, steps)
 collatz' n steps
     | n `mod` 2 == 0 = collatz' (n `div` 2) (steps+1)
     | otherwise = collatz' (3*n + 1) (steps+1)
+
+
+-- "Dig deeper" iterative solution
+collatz2 :: Integer -> Maybe Integer
+    | n < 1 = Nothing 
+    | otherwise = Just(countSteps n)
+    where countSteps = toInteger . length . takeWhile (/=1) . iterate nextStep 
+          nextStep k = if even k then k `div` 2 else 3 * k + 1
